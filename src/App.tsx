c5c3fa9a97e64d52d73cc1e369db97f463729f2e
Benjamin <fellfromatree@gmail.com>
2018-06-28 11:57:29 -0700

    basic dialogue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import './App.css';
+import DialogueLog from './components/DialogLog';
+import Typist from 'react-typist';
 
 class App extends React.Component {
   public render() {
     return (
-      <div />
+      <DialogueLog>
+          <p>I have dialogue!</p>
+          <Typist.Delay ms={500} />
+          <p>I delay!</p>
+          <p>I type gud!<Typist.Backspace count={4} delay={200} />well!</p>
+          <p>(click to skip)This takes foreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeevvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvveeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee<Typist.Delay ms={1000} />eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrrrrrrrrrrr</p>
+      </DialogueLog>
     );
   }
 }
---
+++ src/components/DialogLog.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Typist from 'react-typist';
+import PropTypes from 'prop-types';
+
+
+
+class DialogLog extends React.Component <{
+    onFinish?: PropTypes.func,
+},{
+    typing:boolean,
+}>{
+    state = {
+        typing : true
+    }
+
+    finishTyping = ()=>{
+        this.setState({typing:false})
+    }
+
+    public render() {
+        return (
+            <div className='dialogBox'>
+                {this.state.typing
+                ?
+                    <div onClick={this.finishTyping} className='dialog'>
+                        <Typist cursor={{show:false}} onTypingDone={this.finishTyping}>
+                            {this.props.children}
+                        </Typist>
+                    </div>
+                :
+                    <div onClick={this.props.onFinish} className='dialog'>
+                        {this.props.children}
+                    </div>
+                }
+            </div>
+        );
+    }
+}
+
+export default DialogLog;
