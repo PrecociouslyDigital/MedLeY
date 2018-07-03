1dbc67bd4bd5b40309c5671c319aaf28ecc34819
Benjamin <fellfromatree@gmail.com>
2018-07-03 16:53:25 -0700

    Fix bug where navigate after the global bits don't get passed in.

--- src/dialogSystem/dialogRoot.tsx
+++ src/dialogSystem/dialogRoot.tsx
@@ -97,45 +97,46 @@ export default const dialogRoot : DialogOptionTree  = {
 }
 
 
-
+const errorNode = new DialogNode({
+    title: "You done fucked up",
+    messages : ()=> [
+        {
+            message: (<p> Navigate failed! </p>),
+            sprite:"SadSprite"
+        },
+        {
+            message: (<p> Path was {path} </p>),
+            sprite:"SadSprite"
+        },{
+            message: (<p> Check console for more</p>),
+            sprite:"SadSprite"
+        },{
+            message: (<p> If you're seeing this as a player please tell us about it here (insert link later)</p>),
+            sprite:"SadSprite"
+        }
+    ],
+    children:{},
+    choices: (state) => null,
+    onVisit: (state) => state,
+});
 //Should be of form root://path.to.dialog.node.root/dialog/nodes/navigation
 export const navigate = (path:string) : DialogNode {
     if(path.startsWith("root://")){
         let current : any  = dialogRoot;
-        let bits = path.split("/");
+        let bits = path.split("/",4);
         bits.shift();
         bits.shift(); //gets rid of root://
-        for(let rootBit of (bits.shift() || "").split(".")){
+        for(let rootBit of (bits[2] || "").split(".")){
             //gets the dots path
             current = current[rootBit] as DialogOptionTree;
             //not guarenteed but if this fucks up it should be reproed pretty easily.
         }
         // Look this is blatant abuse of typescript so if it breaks just slap me or smthn idc
-        return current as DialogNode;
+        // I need to stop doing || null checks but it's so convinentttt
+        return (current as DialogNode).navigate(bits[3]) || errorNode;
     }else{
         console.error("Navigate failed! path was " + path);
-        return  new DialogNode({
-            title: "You done fucked up",
-            messages : ()=> [
-                {
-                    message: (<p> Navigate failed! </p>),
-                    sprite:"SadSprite"
-                },
-                {
-                    message: (<p> Path was {path} </p>),
-                    sprite:"SadSprite"
-                },{
-                    message: (<p> Check console for more</p>),
-                    sprite:"SadSprite"
-                },{
-                    message: (<p> If you're seeing this as a player please tell us about it here (insert link later)</p>),
-                    sprite:"SadSprite"
-                }
-            ],
-            children:{},
-            choices: (state) => null,
-            onVisit: (state) => state,
-        });
+        return errorNode;
 
     }
 }
